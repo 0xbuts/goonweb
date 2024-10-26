a790bc7b91d3316a0d6a3c9fa3f382d79d95c257
0xbuts <180214901+0xbuts@users.noreply.github.com>
2024-10-26 19:12:02 -0300

    added main.js with smooth scroll to

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', main);
+
+function main() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
