7abd2dcfbca889c5e051e3207cae0120b2ad565c
0xbuts <180214901+0xbuts@users.noreply.github.com>
2024-10-26 22:30:43 -0300

    added socials

--- assets/js/icons.js
+++ assets/js/icons.js
@@ -1,6 +1,7 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faClipboard, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXTwitter, faTelegram, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faClipboard, faCheck, faXmark);
+library.add(faClipboard, faCheck, faXmark, faXTwitter, faTelegram, faDiscord);
 
 dom.watch();
