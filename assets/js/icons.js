9a3be99b45b7f442d5087c789802bec068ceb1ef
0xbuts <180214901+0xbuts@users.noreply.github.com>
2025-03-31 16:30:32 -0300

    added link to shop

--- assets/js/icons.js
+++ assets/js/icons.js
@@ -1,7 +1,7 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faClipboard, faCheck, faXmark, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard, faCheck, faXmark, faArrowUpRightFromSquare, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { faXTwitter, faTelegram, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faClipboard, faCheck, faXmark, faXTwitter, faTelegram, faDiscord, faArrowUpRightFromSquare);
+library.add(faClipboard, faCheck, faXmark, faXTwitter, faTelegram, faDiscord, faArrowUpRightFromSquare, faShoppingCart);
 
 dom.watch();
